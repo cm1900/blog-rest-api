ee1f798005fef7216428a8cf1b71709c0b2ed7b6
Christino Malebe <cmalebe139@gmail.com>
2025-08-05 20:08:29 +0200

    Pagination support

--- src/main/java/za/co/springhaus/blog/service/PostService.java
+++ src/main/java/za/co/springhaus/blog/service/PostService.java
@@ -1,12 +1,13 @@
 package za.co.springhaus.blog.service;
 
 import za.co.springhaus.blog.dto.PostDto;
+import za.co.springhaus.blog.dto.PostResponse;
 
 import java.util.List;
 
 public interface PostService {
     PostDto createPost(PostDto postDto);
-    List<PostDto> getAllPosts();
+    PostResponse getAllPosts(int pageNo, int pageSize, String sortBy, String sortDir);
     PostDto getPostById(long id);
     PostDto updatePost(PostDto postDto, long id);
     void deletePostById(long id);
--- src/main/java/za/co/springhaus/blog/service/impl/PostServiceImpl.java
+++ src/main/java/za/co/springhaus/blog/service/impl/PostServiceImpl.java
@@ -1,8 +1,12 @@
 package za.co.springhaus.blog.service.impl;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import za.co.springhaus.blog.dto.PostDto;
+import za.co.springhaus.blog.dto.PostResponse;
 import za.co.springhaus.blog.entity.Post;
 import za.co.springhaus.blog.exception.ResourceNotFoundException;
 import za.co.springhaus.blog.repository.PostRepository;
@@ -31,10 +35,30 @@ public class PostServiceImpl implements PostService {
     }
 
     @Override
-    public List<PostDto> getAllPosts() {
+    public PostResponse getAllPosts(int pageNo, int pageSize, String sortBy, String sortDir) {
 
-        List<Post> posts = postRepository.findAll();
-        return posts.stream().map(this::mapToDto).collect(Collectors.toList());
+        Sort sort = sortDir.equalsIgnoreCase(Sort.Direction.ASC.name()) ? Sort.by(sortBy).ascending()
+                : Sort.by(sortBy).descending();
+
+        // create pageable instance
+        Pageable pageable = PageRequest.of(pageNo, pageSize, sort);
+
+        Page<Post> posts = postRepository.findAll(pageable);
+
+        // get content for page object
+        List<Post> listOfPosts = posts.getContent();
+
+        List<PostDto> content = listOfPosts.stream().map(this::mapToDto).collect(Collectors.toList());
+
+        PostResponse postResponse = new PostResponse();
+        postResponse.setContent(content);
+        postResponse.setPageNo(posts.getNumber());
+        postResponse.setPageSize(posts.getSize());
+        postResponse.setTotalElements(posts.getTotalElements());
+        postResponse.setTotalPages(posts.getTotalPages());
+        postResponse.setLast(posts.isLast());
+
+        return postResponse;
     }
 
     @Override
