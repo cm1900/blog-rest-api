6d8496561d1667aa34ff9b4e67c662dda7b31f1c
Christino Malebe <cmalebe139@gmail.com>
2025-09-02 19:20:46 +0200

    Refactor Post service to return all comments along with posts

--- src/main/java/za/co/springhaus/blog/dto/PostDto.java
+++ src/main/java/za/co/springhaus/blog/dto/PostDto.java
@@ -2,10 +2,13 @@ package za.co.springhaus.blog.dto;
 
 import lombok.Data;
 
+import java.util.Set;
+
 @Data
 public class PostDto {
     private long id;
     private String title;
     private String description;
     private String content;
+    private Set<CommentDto> comments;
 }
