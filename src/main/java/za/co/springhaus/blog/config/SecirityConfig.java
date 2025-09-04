617cfb1734a517eeffccfcd918eda800909c530f
Christino Malebe <cmalebe139@gmail.com>
2025-09-04 23:06:21 +0200

    Database authentication

--- src/main/java/za/co/springhaus/blog/config/SecirityConfig.java
+++ src/main/java/za/co/springhaus/blog/config/SecirityConfig.java
@@ -3,7 +3,9 @@ package za.co.springhaus.blog.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -19,9 +21,20 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableMethodSecurity
 public class SecirityConfig {
 
+    private UserDetailsService userDetailsService;
+
+    public SecirityConfig(UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
     @Bean
     public static PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
     }
 
     @Bean
@@ -36,20 +49,4 @@ public class SecirityConfig {
 
         return http.build();
     }
-
-//    @Bean
-//    public UserDetailsService userDetailsService(){
-//        UserDetails admin = User.builder()
-//                .username("admin")
-//                .password(passwordEncoder().encode("admin"))
-//                .roles("ADMIN")
-//                .build();
-//
-//        UserDetails christino = User.builder()
-//                .username("christino")
-//                .password(passwordEncoder().encode("christino"))
-//                .roles("USER")
-//                .build();
-//        return new InMemoryUserDetailsManager(admin, christino);
-//    }
 }
